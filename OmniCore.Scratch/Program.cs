db7cd7ce933f2902d1bf858f45374b6ecbb66456
Barış Kurtlutepe <barisk@gmail.com>
2019-03-07 00:26:58 +0100

    added backscratcher

---
+++ OmniCore.Scratch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace OmniCore.Scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scratch world!");
+
+            var di = new DirectoryInfo(@"D:\omnipod\ti_captures\");
+            foreach (var fi in di.GetFiles("*.psd"))
+            {
+                SnifferUtils.ExtractPacketsFromTISniffer(fi.FullName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OmniCore.Scratch/SnifferUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OmniCore.Scratch
+{
+    public static class SnifferUtils
+    {
+        public static void ExtractPacketsFromTISniffer(string path)
+        {
+            using (var sr = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                var br = new BinaryReader(sr);
+                var streamLength = sr.Length;
+                while (sr.Position < streamLength)
+                {
+                    var packetInformation = br.ReadByte();
+                    var packetNumber = br.ReadUInt32();
+                    var timestamp = br.ReadUInt64();
+                    var packetLength = br.ReadUInt16();
+                    var packetData = br.ReadBytes(2051);
+                }
+            }
+        }
+    }
+}
