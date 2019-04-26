e6e656acdddbf06c7bf2edcd4ea6a42beb797e3a
Barış Kurtlutepe <barisk@gmail.com>
2019-04-26 18:44:27 +0200

    don't do any radio stuff yet

--- OmniCore.Mobile/OmniCore.Mobile/Views/TestPage.xaml.cs
+++ OmniCore.Mobile/OmniCore.Mobile/Views/TestPage.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using nexus.protocols.ble;
-using OmniCore.Model;
-using OmniCore.Model.Protocol.Base;
-using OmniCore.Model.Utilities;
-using OmniCore.Radio.RileyLink;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
 using System;
@@ -17,11 +13,8 @@ namespace OmniCore.Mobile.Views
 {
     public partial class TestPage : ContentPage
     {
-        private readonly IPacketRadio PacketRadio;
-
         public TestPage()
         {
-            this.PacketRadio = new RileyLink(App.Instance.BleAdapter);
             this.BindingContext = this;
             InitializeComponent();
         }
@@ -60,33 +53,10 @@ namespace OmniCore.Mobile.Views
             if (!await CheckPermission(Permission.Storage))
                 return;
 
-            await this.PacketRadio.Initialize();
-
             var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "mytestpdm.json");
             if (File.Exists(path))
                 File.Delete(path);
 
-            var radio = new SoftwareMessageRadio(this.PacketRadio);
-
-            var pdm = Pdm.Load(radio, path);
-            if (pdm == null)
-            {
-                var pod = new Pod() { Lot = 44152, Tid = 1220040, Address = 0x1F0E89F2 };
-                var basalSchedule = new BasalSchedule(
-                    new BasalEntry[] {
-                    new BasalEntry(1m, new TimeSpan(0, 0, 0), new TimeSpan(4, 0, 0)),
-                    new BasalEntry(0.6m, new TimeSpan(4, 0, 0), new TimeSpan(7, 0, 0)),
-                    new BasalEntry(1m, new TimeSpan(7, 0, 0), new TimeSpan(12, 0, 0)),
-                    new BasalEntry(1.6m, new TimeSpan(12, 0, 0), new TimeSpan(15, 0, 0)),
-                    new BasalEntry(1m, new TimeSpan(15, 0, 0), new TimeSpan(17, 0, 0)),
-                    new BasalEntry(0.4m, new TimeSpan(17, 0, 0), new TimeSpan(19, 0, 0)),
-                    new BasalEntry(2m, new TimeSpan(19, 0, 0), new TimeSpan(24, 0, 0))
-                    });
-                pdm = new Pdm(radio, pod, basalSchedule);
-                pdm.Save(path);
-            }
-
-            await pdm.UpdateStatus();
         }
     }
 }
