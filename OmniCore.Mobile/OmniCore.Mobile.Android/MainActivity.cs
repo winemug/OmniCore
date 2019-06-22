cb0dc3815977bf30b9a49c821ab45e6d252c7d39
Barış Kurtlutepe <barisk@gmail.com>
2019-06-22 22:02:03 +0200

    change app label

--- OmniCore.Mobile/OmniCore.Mobile.Android/MainActivity.cs
+++ OmniCore.Mobile/OmniCore.Mobile.Android/MainActivity.cs
@@ -18,7 +18,7 @@ using Environment = System.Environment;
 
 namespace OmniCore.Mobile.Android
 {
-    [Activity(Label = "OmniCore.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "OmniCore", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
