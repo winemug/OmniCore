d80482b569ba82b765bbf8f75785016a62d3d603
Barış Kurtlutepe <barisk@gmail.com>
2019-07-17 16:09:12 +0200

    radio settings wip

---
+++ OmniCore.Mobile/OmniCore.Mobile/ViewModels/Settings/RadioSettingsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OmniCore.Mobile.ViewModels.Settings
+{
+    public class RadioSettingsViewModel : PageViewModel
+    {
+        public RadioSettingsViewModel(Page page) : base(page)
+        {
+        }
+
+        protected override void OnDisposeManagedResources()
+        {
+        }
+
+        protected override async Task<BaseViewModel> BindData()
+        {
+            return this;
+        }
+    }
+}
---
+++ OmniCore.Mobile/OmniCore.Mobile/Views/Settings/RadioSettingsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OmniCore.Mobile.ViewModels.Settings;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OmniCore.Mobile.Views.Settings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RadioSettingsPage : ContentPage
+    {
+        public RadioSettingsPage()
+        {
+            InitializeComponent();
+            new RadioSettingsViewModel(this);
+        }
+    }
+}
