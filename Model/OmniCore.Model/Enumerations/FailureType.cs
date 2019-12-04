582e4892ac8e2c1d178fc6e51cfb62773642037e
Barış Kurtlutepe <barisk@gmail.com>
2019-12-04 20:36:35 +0100

    add repositoryexception

--- Model/OmniCore.Model/Enumerations/FailureType.cs
+++ Model/OmniCore.Model/Enumerations/FailureType.cs
@@ -23,5 +23,6 @@ namespace OmniCore.Model.Enumerations
         WakeLockNotAcquired,
         SystemError,
         Unknown,
+        LocalStorage
     }
 }
---
+++ Model/OmniCore.Model/Exceptions/OmniCoreRepositoryException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OmniCore.Model.Enumerations;
+
+namespace OmniCore.Model.Exceptions
+{
+    public class OmniCoreRepositoryException : OmniCoreException
+    {
+        public OmniCoreRepositoryException(FailureType failureType, string message = "Unknown", Exception inner = null) : base(failureType, message, inner)
+        {
+        }
+    }
+}
