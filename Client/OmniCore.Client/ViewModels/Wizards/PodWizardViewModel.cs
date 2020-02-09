046a783cfa2800aeb7371d5274d8146e688c7c61
Barış Kurtlutepe <barisk@gmail.com>
2020-02-09 21:54:27 +0100

    fix routes

--- Client/OmniCore.Client/ViewModels/Wizards/PodWizardViewModel.cs
+++ Client/OmniCore.Client/ViewModels/Wizards/PodWizardViewModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using OmniCore.Client.Platform;
 using OmniCore.Client.ViewModels.Base;
 using OmniCore.Client.Views.Wizards.NewPod;
+using OmniCore.Model.Enumerations;
 using OmniCore.Model.Interfaces.Client;
 using Xamarin.Forms;
 
@@ -19,21 +22,40 @@ namespace OmniCore.Client.ViewModels.Wizards
         public ICommand NextPageCommand { get; private set; }
         public ContentView ActiveView { get; private set; }
 
-        private PodWizardCreationOptions Page1 = new PodWizardCreationOptions();
-        private PodWizardExistingPodOptions Page2 = new PodWizardExistingPodOptions();
+        private PodWizardCreationOptions CreationOptionsView = new PodWizardCreationOptions();
+        private PodWizardExistingPodOptions ExistingInActivationView = new PodWizardExistingPodOptions();
         private PodWizardAdvancedOptions Page3 = new PodWizardAdvancedOptions();
-        private PodWizardPodTypeSelection Page4 = new PodWizardPodTypeSelection();
         private PodWizardIntegrationOptions Page5 = new PodWizardIntegrationOptions();
         private PodWizardMedicationSelection Page6 = new PodWizardMedicationSelection();
         private PodWizardRadioSelection Page7 = new PodWizardRadioSelection();
 
-        public bool OptionActivateNewPod { get; set; }
-
         public PodWizardViewModel(ICoreClient client) : base(client)
         {
-            ActiveView = Page1;
             NextPageCommand = new Command(async () => { await GoToNextPage(); });
         }
+
+        protected override async Task OnPageAppearing()
+        {
+            if (Parameter == null)
+            {
+                var activePods = await Api.CorePodService.ActivePods(CancellationToken.None);
+                if (activePods.Any(p => p.RunningState.State <= PodState.Started))
+                {
+                    ActiveView = ExistingInActivationView;
+                }
+                else
+                {
+                    ActiveView = CreationOptionsView;
+                }
+            }
+            else
+            {
+                //
+            }
+
+            await base.OnPageAppearing();
+        }
+
         private async Task GoToNextPage()
         {
             //var nextPage = NextPageFunction?.Invoke();
