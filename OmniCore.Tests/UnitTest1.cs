1a9df7db0b3ad956c60c91df819a2eb054691003
Baris Kurtlutepe <barisk@gmail.com>
2023-03-17 14:01:37 +0100

    tests project

---
+++ OmniCore.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OmniCore.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
