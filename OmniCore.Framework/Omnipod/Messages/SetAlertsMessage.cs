f97c832ed11c80183a061fcb99f924d3deb2abd6
Baris Kurtlutepe <barisk@gmail.com>
2023-04-21 21:49:27 +0200

    set alerts

---
+++ OmniCore.Framework/Omnipod/Messages/SetAlertsMessage.cs
@@ -0,0 +1,78 @@
+﻿using OmniCore.Common.Pod;
+using OmniCore.Services;
+using OmniCore.Services.Interfaces.Entities;
+using OmniCore.Services.Interfaces.Pod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OmniCore.Framework.Omnipod.Messages;
+
+public class SetAlertsMessage : IMessageData
+{
+    public List<AlertConfiguration> AlertConfigurations { get; set; }
+
+    public static Predicate<IMessageParts> CanParse => (parts) => parts.MainPart.Type == PodMessagePartType.RequestConfigureAlerts;
+
+    public IMessageData FromParts(IMessageParts parts)
+    {
+        AlertConfigurations = new List<AlertConfiguration>();
+        var idx = 0;
+        while (idx < parts.MainPart.Data.Length)
+        {
+            var b0 = parts.MainPart.Data[idx + 0];
+            var b1= parts.MainPart.Data[idx + 1];
+            var u0 = parts.MainPart.Data.Word(idx + 2);
+            var b2 = parts.MainPart.Data[idx + 4];
+            var b3 = parts.MainPart.Data[idx + 5];
+
+            var ac = new AlertConfiguration
+            {
+                AlertIndex = b0 >> 4,
+                SetActive = (b0 & 0x08) > 0,
+                ReservoirBased = (b0 & 0x04) > 0,
+                SetAutoOff = (b0 & 0x02) > 0,
+                AlertDurationMinutes = b1 + ((b0 & 0x01) << 8),
+                AlertAfter = u0 & 0x3FFF,
+                BeepPattern = (BeepPattern)b2,
+                BeepType = (BeepType)b3
+            };
+
+            AlertConfigurations.Add(ac);
+            idx += 6;
+        }
+        return this;
+    }
+
+    public IMessageParts ToParts()
+    {
+        var data = new Bytes();
+        foreach (var alertConfiguration in AlertConfigurations)
+        {
+            var b0 = (alertConfiguration.AlertIndex & 0x07) << 4;
+            b0 |= alertConfiguration.SetActive ? 0x08 : 0x00;
+            b0 |= alertConfiguration.ReservoirBased ? 0x04 : 0x00;
+            b0 |= alertConfiguration.SetAutoOff ? 0x02 : 0x00;
+
+            var d = alertConfiguration.AlertDurationMinutes & 0x1FF;
+            b0 |= d >> 8;
+            var b1 = d & 0xFF;
+            data.Append((byte)b0).Append((byte)b1);
+
+            var u0 = alertConfiguration.AlertAfter & 0x3FFF;
+            var b2 = (byte)alertConfiguration.BeepPattern;
+            var b3 = (byte)alertConfiguration.BeepType;
+            data.Append((ushort)u0).Append(b2).Append(b3);
+        }
+        return new MessageParts(
+            new MessagePart
+            {
+                Type = PodMessagePartType.RequestConfigureAlerts,
+                RequiresNonce = true,
+                Data = data
+            }); ;
+    }
+}
