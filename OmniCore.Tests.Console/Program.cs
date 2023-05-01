02ace3d90aa99ff91f915b4a37d2dc2674a725bc
Baris Kurtlutepe <barisk@gmail.com>
2023-05-01 17:37:57 +0200

    local parse test

--- OmniCore.Tests.Console/Program.cs
+++ OmniCore.Tests.Console/Program.cs
@@ -5,9 +5,13 @@ using System.Runtime.InteropServices;
 using System.Text.Json;
 using MessagePack;
 using MessagePack.Resolvers;
+using OmniCore.Common.Data;
+using OmniCore.Common.Pod;
 using OmniCore.Framework.Omnipod;
 using OmniCore.Framework.Omnipod.Messages;
 using OmniCore.Framework.Omnipod.Requests;
+using OmniCore.Framework.Omnipod.Responses;
+using OmniCore.Services.Interfaces.Entities;
 using OmniCore.Services.Interfaces.Pod;
 //using Moq;
 //using OmniCore.Common.Data;
@@ -18,21 +22,50 @@ using OmniCore.Services.Interfaces.Pod;
 //using OmniCore.Tests;
 //using Plugin.BLE.Abstractions.Extensions;
 
+using var fs = new FileStream("d:\\pa.json", FileMode.Open, FileAccess.Read);
+var pas = await JsonSerializer.DeserializeAsync<SPodAction[]>(fs);
 
+DateTimeOffset? tsMin = null;
+DateTimeOffset? tsMax = null;
+long totalTs = 0;
+int countTs = 0;
 
-var message = new MessageBuilder()
-    .WithSequence(0)
-    .WithAddress(0x01020304)
-    .Build(new SetBeepingMessage
+foreach(var pa in pas)
+{
+    var sent = new object();
+    var received = new object();
+    if (pa.SentData != null)
+        sent = new MessageBuilder().Build(new Bytes(pa.SentData)).Data;
+
+    if (pa.ReceivedData != null)
+        received = new MessageBuilder().Build(new Bytes(pa.ReceivedData)).Data;
+
+    //var sSent = JsonSerializer.Serialize(sent);
+    //var sReceived = JsonSerializer.Serialize(received);
+    //Console.WriteLine($"\n{sent.GetType().Name} Idx: {pa.Index} {pa.RequestSentLatest} <-> {pa.RequestSentEarliest}");
+    //Console.WriteLine($"Sent: {sSent}");
+    //Console.WriteLine($"{received.GetType().Name}");
+    //Console.WriteLine($"Received: {sReceived}");
+
+    if (received is StatusMessage sm)
     {
-        BeepNow = BeepType.BeepBeep
-    });
+        if (sm.ProgressModel.Faulted)
+            continue;
+        var ts = pa.RequestSentEarliest.Value;
+        var mins = sm.StatusModel.ActiveMinutes;
+        var tsp = ts - TimeSpan.FromMinutes(mins);
+        if (!(tsMin < tsp))
+            tsMin = tsp;
+        if (!(tsMax > tsp))
+            tsMax = tsp;
 
-Console.WriteLine(JsonSerializer.Serialize(message));
-
-var message2 = new MessageBuilder().Build(message.Body);
-
-Console.WriteLine(JsonSerializer.Serialize(message2));
+        totalTs += tsp.ToUnixTimeSeconds();
+        countTs++;
+    }
+}
+var meanTs = (totalTs / countTs) - 30;
+var tsMean = DateTimeOffset.FromUnixTimeSeconds(meanTs);
+Debug.WriteLine($"Min: {tsMin} Max: {tsMax} Mean: {tsMean}");
 
 
 // {"MyProperty1":99,"MyProperty2":9999}
@@ -74,3 +107,16 @@ Console.WriteLine(JsonSerializer.Serialize(message2));
 //         Parts = msgParts
 //     };
 // }
+
+
+class SPodAction
+{
+    public Guid PodId { get; set; }
+    public int Index { get; set; }
+    public Guid? ClientId { get; set; }
+    public DateTimeOffset? RequestSentEarliest { get; set; }
+    public DateTimeOffset? RequestSentLatest { get; set; }
+    public byte[]? SentData { get; set; }
+    public byte[]? ReceivedData { get; set; }
+    public AcceptanceType Result { get; set; }
+}
