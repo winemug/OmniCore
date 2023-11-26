83f5294a722ca8461a62e74d5c362692f6969135
Baris Kurtlutepe <barisk@gmail.com>
2023-11-27 00:14:45 +0100

    add mvvm and maui community toolkits

--- OmniCore.Client/MauiProgram.cs
+++ OmniCore.Client/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using OmniCore.Client.Platforms;
 
 namespace OmniCore.Client
@@ -10,6 +11,7 @@ namespace OmniCore.Client
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
