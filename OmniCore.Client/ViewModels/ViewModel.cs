e3bc34cff1e944081ad652f8d77a4545cb10b59e
Baris Kurtlutepe <barisk@gmail.com>
2023-11-27 00:24:18 +0100

    base viewmodel

---
+++ OmniCore.Client/ViewModels/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniCore.Client.ViewModels;
+
+public class ViewModel : ObservableObject
+{
+    public virtual Task OnResumed() => Task.CompletedTask;
+    public virtual Task OnPaused() => Task.CompletedTask;
+
+    public virtual Task OnNavigatingTo() => Task.CompletedTask;
+
+    public virtual Task OnNavigatingAway() => Task.CompletedTask;
+
+    public virtual Task BindToView(Page page)
+    {
+        page.BindingContext = this;
+        return Task.CompletedTask;
+    }
+}
