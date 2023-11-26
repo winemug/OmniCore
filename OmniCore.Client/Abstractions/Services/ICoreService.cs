9f77c1701ddec45fc29147dfbd9108f41268a5ec
Baris Kurtlutepe <barisk@gmail.com>
2023-11-26 22:05:17 +0100

    inline abstractions

---
+++ OmniCore.Client/Abstractions/Services/ICoreService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniCore.Client.Abstractions.Services;
+public interface ICoreService
+{
+    IDisposable ForegroundActivityContext();
+    Task InteractiveStartAsync();
+    Task OnCreatedAsync();
+    Task OnActivatedAsync();
+    Task OnDeactivatedAsync();
+    Task OnStoppedAsync();
+    Task OnResumedAsync();
+    Task OnDestroyingAsync();
+}
---
+++ OmniCore.Client/Abstractions/Services/ISettingsService.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace OmniCore.Client.Abstractions.Services;
+
+public interface ISettingsService : INotifyPropertyChanged
+{
+    ClientAuthorization? Authorization { get; set; }
+    AmqpEndpointDefinition? EndpointDefinition { get; set; }
+}
+
+public record ClientAuthorization
+{
+    public Guid ClientId { get; init; }
+    public byte[] Token { get; init; }
+}
+
+public class AmqpEndpointDefinition
+{
+    public string UserId { get; set; }
+    public string Dsn { get; set; }
+    public string Queue { get; set; }
+    public string Exchange { get; set; }
+}
