924fae2ddc863af6c5d8ee9c198cf3e7ed561869
Baris Kurtlutepe <barisk@gmail.com>
2023-12-09 22:26:11 +0100

    cm

--- OmniCore.Shared/Entities/Omnipod/Parts/RequestCancelDelivery.cs
+++ OmniCore.Shared/Entities/Omnipod/Parts/RequestCancelDelivery.cs
@@ -6,7 +6,7 @@ namespace OmniCore.Shared.Entities.Omnipod.Parts;
 public class RequestCancelDelivery : IMessagePart
 {
      public BeepType Beep { get; set; }
-     // public bool StopExtendedBolus { get; set; }
+     public bool StopExtendedBolus { get; set; }
      public bool StopBolus { get; set; }
      public bool StopTempBasal { get; set; }
      public bool StopBasal { get; set; }
@@ -15,7 +15,7 @@ public class RequestCancelDelivery : IMessagePart
         var b0 = span[0];
         return new RequestCancelDelivery
         {
-            //StopExtendedBolus = (b0 & 0x08) > 0,
+            StopExtendedBolus = (b0 & 0x08) > 0,
             StopBolus = (b0 & 0x04) > 0,
             StopTempBasal = (b0 & 0x02) > 0,
             StopBasal = (b0 & 0x01) > 0,
@@ -25,7 +25,7 @@ public class RequestCancelDelivery : IMessagePart
     public int ToBytes(Span<byte> span)
     {
         span.WriteBits((int)Beep, 0, 4);
-        //span.WriteBits(StopExtendedBolus ? 1: 0,4, 1);
+        span.WriteBits(StopExtendedBolus ? 1: 0,4, 1);
         span.WriteBits(StopBolus ? 1: 0,5, 1);
         span.WriteBits(StopTempBasal ? 1: 0,6, 1);
         span.WriteBits(StopBasal ? 1: 0,7, 1);
